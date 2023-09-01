39d201c8d92b2c9e157cd9eccd7fc8b2c25cdfd7
hclanka <ot.gaming132@gmail.com>
2023-09-01 21:13:01 +0530

    .

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import deno from "@astrojs/deno"
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   integrations: [tailwind()],
+  adapter: deno()
 });
